0275512d7c2a3d23c7cb9158368f3762df75e711
Morgan Corbridge <pcpower@uw.edu>
2021-04-29 11:50:50 -0700

    BlogData works

---
+++ blog-homepage/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import BlogHomepage from './Blog-Homepage/BlogHomepage';
+
+
+
+
+function App() {
+  return (
+  <div className="App">
+    <BlogHomepage />
+  </div>
+  );
+  
+}
+
+export default App;
---
+++ blog-homepage/src/Blog-Homepage/BlogHomepage.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import "./Blog.css";
+import { BlogData, BlogDataYourArticles } from './BlogData'
+
+
+class BlogHomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Blogs: [
+                ''
+            ]
+        };
+    }
+
+    
+
+    render() {
+        
+        return (
+            <>
+            <header className="header">
+                <h2>For you</h2>
+            </header>
+        
+            <div className="blog-container">
+                {BlogData.map((BlogData, key) => {
+                    return (
+                        <div key={key}>
+
+                            
+                          
+                            <img src={BlogData.image} />
+                                <h5>
+                                {BlogData.title} {BlogData.description}
+                                
+                                </h5>
+                                        <a href={BlogData.link}>Read More</a>
+                                <h5>
+                                    <img src={BlogData.author.image}/>                            
+                                </h5>
+                            <h3>
+                                {BlogData.author.name}
+                                
+                            </h3>                             
+                        </div>
+                                
+                                                            
+                                
+                                
+
+
+        
+                    );
+                })}
+                
+            </div>
+        </>
+        )
+    }
+}
+
+export default BlogHomePage
